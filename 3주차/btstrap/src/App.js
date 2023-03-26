23e91d7b1d61b2606276ee9b6d3842839b03e932
shlee9999 <oklshop555@naver.com>
2023-03-26 21:17:39 +0900

    3주차_과제_수정1

--- 3주차/btstrap/src/App.js
+++ 3주차/btstrap/src/App.js
@@ -1,5 +1,28 @@
 import './App.css';
+export default App;
+
+function Card(props) {
+
+  return <div className="col mb-4">
+    <div className="card shadow-lg">
+      <div className="card-header">
+        <h4>{props.title}</h4>
+      </div>
+      <div className="card-body">
+        <h1><b>{props.cost}</b> <small className="text-muted">/ mo</small></h1>
+        <ul className="list-unstyled mt-3 mb-4">
+          <li>{props.usernum} users included</li>
+          <li>{props.storage} GB of storage</li>
+          <li>{props.support} support</li>
+          <li>{props.access}Help center access</li>
+        </ul>
+        <button className={`btn ${props.color} btn-lg`}>{props.btnBody}</button>
+      </div>
+    </div>
+  </div>
+}
 function App() {
+
   return (
     <div className="App">
       <div className="container text-center py-5">
@@ -14,59 +37,12 @@ function App() {
 
       <div className="container text-center w-100">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-          <div className="col mb-4">
-            <div className="card shadow-lg">
-              <div className="card-header">
-                <h4>Free</h4>
-              </div>
-              <div className="card-body">
-                <h1><b>$0</b> <small className="text-muted">/ mo</small></h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>10 users included</li>
-                  <li>2 GB of storage</li>
-                  <li>Email support</li>
-                  <li>Help center access</li>
-                </ul>
-                <button className="btn btn-outline-primary btn-lg">Sign up for free</button>
-              </div>
-            </div>
-          </div>
-          <div className="col mb-4">
-            <div className="card shadow-lg">
-              <div className="card-header">
-                <h4>Pro</h4>
-              </div>
-              <div className="card-body">
-                <h1><b>$15</b> <small className="text-muted">/ mo</small></h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>20 users included</li>
-                  <li>10 GB of storage</li>
-                  <li>Priority email support</li>
-                  <li>Help center access</li>
-                </ul>
-                <button className="btn btn-primary btn-lg">Get started</button>
-              </div>
-            </div>
-          </div>
-          <div className="col mb-4">
-            <div className="card shadow-lg">
-              <div className="card-header">
-                <h4>Enterprise</h4>
-              </div>
-              <div className="card-body">
-                <h1><b>$29</b><small className="text-muted">/ mo</small></h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>30 users included</li>
-                  <li>15 GB of storage</li>
-                  <li>Phone and email support</li>
-                  <li>Help center access</li>
-                </ul>
-                <button className="btn btn-primary btn-lg">Contact us</button>
-              </div>
-            </div>
-          </div>
+          <Card title="Free" usernum="10" color="btn-outline-primary" cost="$0" storage="2" support="Email" btnBody="Sign up for free"></Card>
+          <Card title="Pro" usernum="20" color="btn-primary" cost="$15" storage="10" support="Priority email" btnBody="Get started"></Card>
+          <Card title="Pro" usernum="30" color="btn-primary" cost="$29" storage="15" support="Priority email" btnBody="Contact us"></Card>
         </div>
       </div>
+
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
         crossorigin="anonymous">
@@ -77,7 +53,6 @@ function App() {
 
 }
 
-export default App;
 
 
 
